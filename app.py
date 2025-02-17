5ec14d65bf4bd14ec19fa008f614b5fcb75b6e4c
deepakdev9797 <deepakdev9797@gmail.com>
2025-02-16 23:07:00 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import os
 import yaml
 from yaml.loader import SafeLoader
 
-with open('./config.yaml') as file:
+with open('../config.yaml') as file:
     config = yaml.load(file, Loader=SafeLoader)
 
 authenticator = stauth.Authenticate(
